cf6b578c66be7fc2ce63a409959b1235efd71b3b
janwa039 <jaibom2@gmail.com>
2019-06-16 12:19:07 -0400

    Chsnged the layout for nthe about me page

--- src/javascript/layout/aboutPage.js
+++ src/javascript/layout/aboutPage.js
@@ -13,27 +13,25 @@ export default class SecondPage extends React.Component{
         <div className ="inner-page-1">
             AFFIONG BASSEY
         </div>
-
         <div className = "aboutme">
-        <div class="my-image">
-          <img src={require('../../images/WhatsApp Image 2018-10-19 at 7.03.58 AM.jpeg')}  alt ='my-image'/>
-        </div>
-        <p>I' am a Software Engineer who is very passionate about growing, 
-        learning something new. Also want to be in a position where i can also help others grow years 
-        from now following this career path. I love building websites and Designing.
-        Skills:Javascript, HTML, CSS, reactJS, Adobe photoshop/sketch.
-        Graduated from university of Ottawa with a Bachelor's Degree in Computer Engineering 
-          (minor in Engineering management and Entrepreneurship) 
-          </p>
-        <div className="middle-second-text-area-second">
+          <div class="my-image">
+            <img src={require('../../images/WhatsApp Image 2018-10-19 at 7.03.58 AM.jpeg')}  alt ='my-profile-image'/>
+          </div >
+          <div className="textfield"> <p>I' am a Software Engineer who is very passionate about growing, 
+              learning something new. Also want to be in a position where i can also help others grow years 
+              from now following this career path. I love building websites and Designing.
+              Skills:Javascript, HTML, CSS, reactJS, Adobe photoshop/sketch.
+              Graduated from university of Ottawa with a Bachelor's Degree in Computer Engineering 
+                (minor in Engineering management and Entrepreneurship) 
+          </p></div>
+          <div className="middle-second-text-area-second">
               <div className="icon-second-container ">
                 <a className="icon-btn" href="#">
                     <i class="fas fa-envelope"></i>
                 </a>
               </div>
         </div>
-              
-      </div>
+        </div>
       </div>
     )
   }
